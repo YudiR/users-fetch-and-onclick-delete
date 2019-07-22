d20ef2d63bc5dbaddc36d26cd9b40f56f3cd2bae
YudiR <yudrose13@gmail.com>
2019-07-22 15:45:22 -0400

    fetch of users. using users names as state. maping state. on click to delete letter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Users from "./components/users";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  delete = index => {
+    console.log("this is the index!!",index);
+    let dataCopy = JSON.parse(JSON.stringify(this.state.users));
+    dataCopy.splice(index,1)
+    this.setState(
+      {
+        users: dataCopy
+      },
+      console.log(this.state.users, "state::::delete:::::::")
+    );
+
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json()) // Transform the data into json
+      .then(data => {
+        let userArray = data.map((e, i) => e.name);
+        console.log(userArray);
+
+        this.setState(
+          {
+            users: userArray
+          },
+          console.log(this.state.users, "state:::::::::::")
+        );
+      });
+  }
+
+  render() {
+    console.log(this.state.users);
+    return (
+      <div className="App">
+        {this.state.users.map((user, index) => (
+          <Users key={index} name={user} index={index} delete={this.delete} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
